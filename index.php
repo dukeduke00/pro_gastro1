1bf20b57983cb3cfe19626c75d671f2d2613fb37
dukeduke00 <dusangostic69@gmail.com>
2024-02-27 17:29:58 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel='stylesheet' href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css'>
+    <title>Document</title>
+   
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+          <i class='bx bx-menu'></i>
+          <div class="logo"><a href="#">PRO GASTRO</a></div>
+          <div class="nav-links">
+            <div class="sidebar-logo">
+              <span class="logo-name">PRO GASTRO</span>
+              <i class='bx bx-x' ></i>
+            </div>
+            <ul class="links">
+              <li><a href="#">O NAMA</a></li>
+              
+              <div>
+              <li >
+                <a class="products"  href="#">PROIZVODI</a>
+                <i class='bx bxs-chevron-down htmlcss-arrow arrow  '></i>
+                <ul class="htmlCss-sub-menu sub-menu">
+                    <li class="more">
+                        <span><a href="#">Nozevi</a>
+                          <i class='bx bxs-chevron-right arrow more-arrow'></i>
+                        </span>
+                        <ul class="more-sub-menu sub-menu">
+                          
+                          <li><a href="#">Swibo</a></li>
+                          <li><a href="#">Victorinox</a></li>
+                          <li><a href="#">Dick</a></li>
+                          <li><a href="#">Swibo</a></li>
+                          <li><a href="#">Victorinox</a></li>
+                        </ul>
+                      </li>
+
+                     
+
+                      <li class="more">
+                        <span><a href="#">Masine</a>
+                          <i class='bx bxs-chevron-right arrow more-arrow'></i>
+                        </span>
+                        <ul class="more-sub-menu sub-menu">
+                          
+                          <li><a href="#">Swibo</a></li>
+                          <li><a href="#">Victorinox</a></li>
+                          <li><a href="#">Dick</a></li>
+                          <li><a href="#">Swibo</a></li>
+                          <li><a href="#">Victorinox</a></li>
+                        </ul>
+                      </li>
+                  <li><a href="#">HTZ Oprema</a></li>
+                  <li><a href="#">Ostraci</a></li>
+
+                  <li class="more">
+                        <span><a href="#">Ostalo</a>
+                          <i class='bx bxs-chevron-right arrow more-arrow'></i>
+                        </span>
+                        <ul class="more-sub-menu sub-menu">
+                          
+                          <li><a href="#">Swibo</a></li>
+                          <li><a href="#">Victorinox</a></li>
+                          <li><a href="#">Dick</a></li>
+                          <li><a href="#">Swibo</a></li>
+                          <li><a href="#">Victorinox</a></li>
+                        </ul>
+                      </li>
+                  
+                </ul>
+
+                
+                
+              </li>
+
+             
+                
+              </li>
+
+              </div>
+              
+              <li><a href="#">KONTAKT</a></li>
+              <li><a href="#">KORPA</a>
+              <i class='bx bx-shopping-bag bx-flip-horizontal' style='color:#ffffff; margin-left:6px; margin-bottom:2px;' ></i>
+            </li>
+            </ul>
+          </div>
+          <div class="search-box">
+            <i class='bx bx-search'></i>
+            <div class="input-box">
+              <input type="text" placeholder="Search...">
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      
+      <script src="js/apps.js"></script>
+</body>
+</html>
